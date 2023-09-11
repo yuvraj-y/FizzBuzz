874772461376078bae9757de76e8aa3d0571c01b
yuvraj-y <yuvraj.yadav@mail.utronto.ca>
2023-09-11 10:06:44 -0400

    Main.java created

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,11 +7,11 @@ class FizzBuzz {
         int i=1;
         while (i <= 100) {
 
-            i = doFizzBuzz(i);
+            i = doFizzBuzz(5);
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
